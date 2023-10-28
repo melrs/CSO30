ae00a53140c7f6a5e1eea920d0bdad497054c1e2
melrs <melissafrnn@gmail.com>
2023-10-28 00:28:59 -0300

    Create timer

--- PingPongOs/ppos-core-aux.c
+++ PingPongOs/ppos-core-aux.c
@@ -6,6 +6,14 @@
 // Coloque aqui as suas modificações, p.ex. includes, defines variáveis, 
 // estruturas e funções
 //#define DEBUG 1;
+#include <signal.h>
+#include <sys/time.h>
+#include <stdbool.h>
+
+// estrutura de inicialização do timer
+struct itimerval timer;
+struct sigaction action;
+bool running_system_task = false;
 
 task_t * chooseTask(task_t *task) {
     if (task == NULL)
@@ -84,18 +92,53 @@ void print_tcb(task_t* task){
     printf("(%d,%d), ", task->id, task->priority);
 }
 
+
+void handler (int signum) {
+  systemTime++;
+  if(!running_system_task)
+    taskExec->quantum--;
+  
+  if(taskExec->quantum == 0)
+    task_yield();
+
+}
+
+void temporizer () {
+  action.sa_handler = handler;
+  sigemptyset (&action.sa_mask);
+  action.sa_flags = 0;
+  if (sigaction (SIGALRM, &action, NULL) < 0) {
+    perror ("Erro em sigaction: ");
+    exit (1) ;
+  }
+
+  timer.it_value.tv_usec = 25000;      // primeiro disparo, em micro-segundos
+  timer.it_value.tv_sec  = 0;      // primeiro disparo, em segundos
+  timer.it_interval.tv_usec = 1000;   // disparos subsequentes, em micro-segundos
+  timer.it_interval.tv_sec  = 0;   // disparos subsequentes, em segundos
+
+  if (setitimer (ITIMER_REAL, &timer, NULL) < 0) {
+    perror ("Erro em setitimer: ");
+    exit (1);
+  }
+
+}
+
 // ****************************************************************************
 
 
 
 void before_ppos_init () {
-    // put your customization here
+    running_system_task = true;
+    systemTime = 0;
+    temporizer();
 #ifdef DEBUG
     printf("\ninit - BEFORE");
 #endif
 }
 
 void after_ppos_init () {
+    running_system_task = false;
     // put your customization here
 #ifdef DEBUG
     printf("\ninit - AFTER");
@@ -103,7 +146,7 @@ void after_ppos_init () {
 }
 
 void before_task_create (task_t *task ) {
-
+    running_system_task = true;
 #ifdef DEBUG
     printf("\ntask_create - BEFORE - [%d]", task->id);
     printf("cria uma nova tarefa, que recebe como parâmetros uma funcao e um argumento\n");
@@ -111,6 +154,7 @@ void before_task_create (task_t *task ) {
 }
 
 void after_task_create (task_t *task ) {
+    running_system_task = false;
     task_set_eet(task, 99999);
 #ifdef DEBUG
     printf("\ntask_create - AFTER - [%d]", task->id);
@@ -118,7 +162,7 @@ void after_task_create (task_t *task ) {
 }
 
 void before_task_exit () {
-    // put your customization here
+    running_system_task = true;
 #ifdef DEBUG
     printf("\ntask_exit - BEFORE - [%d]", taskExec->id);
     printf("termina a tarefa corrente, indicando um valor de status encerramento\n");
@@ -126,6 +170,7 @@ void before_task_exit () {
 }
 
 void after_task_exit () {
+    running_system_task = false;
     // put your customization here
 #ifdef DEBUG
     printf("\ntask_exit - AFTER- [%d]", taskExec->id);
@@ -133,6 +178,7 @@ void after_task_exit () {
 }
 
 void before_task_switch ( task_t *task ) {
+    running_system_task = true;
     // put your customization here
 #ifdef DEBUG
     printf("\ntask_switch - BEFORE - [%d -> %d]", taskExec->id, task->id);
@@ -141,6 +187,7 @@ void before_task_switch ( task_t *task ) {
 }
 
 void after_task_switch ( task_t *task ) {
+    running_system_task = false;
     // put your customization here
 #ifdef DEBUG
     printf("\ntask_switch - AFTER - [%d -> %d]", taskExec->id, task->id);
@@ -149,6 +196,7 @@ void after_task_switch ( task_t *task ) {
 }
 
 void before_task_yield () {
+    running_system_task = true;
     // put your customization here
 #ifdef DEBUG
     printf("\ntask_yield - BEFORE - [%d]", taskExec->id);
@@ -156,6 +204,7 @@ void before_task_yield () {
 #endif
 }
 void after_task_yield () {
+    running_system_task = false;
     // put your customization here
 #ifdef DEBUG
     printf("\ntask_yield - AFTER - [%d]", taskExec->id);
@@ -164,6 +213,7 @@ void after_task_yield () {
 
 
 void before_task_suspend( task_t *task ) {
+    running_system_task = true;
     // put your customization here
 #ifdef DEBUG
     printf("\ntask_suspend - BEFORE - [%d]", task->id);
@@ -172,6 +222,7 @@ void before_task_suspend( task_t *task ) {
 }
 
 void after_task_suspend( task_t *task ) {
+    running_system_task = false;
     // put your customization here
 #ifdef DEBUG
     printf("\ntask_suspend - AFTER - [%d]", task->id);
@@ -179,6 +230,7 @@ void after_task_suspend( task_t *task ) {
 }
 
 void before_task_resume(task_t *task) {
+    running_system_task = true;
     // put your customization here
 #ifdef DEBUG
     printf("\ntask_resume - BEFORE - [%d]", task->id);
@@ -187,6 +239,7 @@ void before_task_resume(task_t *task) {
 }
 
 void after_task_resume(task_t *task) {
+    running_system_task = false;
     // put your customization here
 #ifdef DEBUG
     printf("\ntask_resume - AFTER - [%d]", task->id);
@@ -194,6 +247,7 @@ void after_task_resume(task_t *task) {
 }
 
 void before_task_sleep () {
+    running_system_task = true;
     // put your customization here
 #ifdef DEBUG
     printf("\ntask_sleep - BEFORE - [%d]", taskExec->id);
@@ -202,6 +256,7 @@ void before_task_sleep () {
 }
 
 void after_task_sleep () {
+    running_system_task = false;
     // put your customization here
 #ifdef DEBUG
     printf("\ntask_sleep - AFTER - [%d]", taskExec->id);
@@ -209,6 +264,7 @@ void after_task_sleep () {
 }
 
 int before_task_join (task_t *task) {
+    running_system_task = true;
     // put your customization here
 #ifdef DEBUG
     printf("\ntask_join - BEFORE - [%d]", taskExec->id);
@@ -220,6 +276,7 @@ int before_task_join (task_t *task) {
 }
 
 int after_task_join (task_t *task) {
+    running_system_task = false;
     // put your customization here
 #ifdef DEBUG
     printf("\ntask_join - AFTER EXECUTANDO:- [%d]", taskExec->id);
@@ -504,6 +561,7 @@ int after_mqueue_msgs (mqueue_t *queue) {
 
 // Uma função scheduler que analisa a fila de tarefas prontas, devolvendo um ponteiro para a próxima tarefa a receber o processador, que será a com menor prioridade.
 task_t * scheduler() {
+    running_system_task = true;
     //printf("\nCHAMANDO O SCHEDULER\n");
     //PRINT_READY_QUEUE
     if(readyQueue == NULL)
@@ -518,7 +576,8 @@ task_t * scheduler() {
             lowestPriority = next;
         }
     }
-
+    running_system_task = false;
+    lowestPriority->quantum = 20;
     //printf("\nSCHEDULER RETORNOU [%d]\n", lowestPriority->id);
     return lowestPriority;
 }
